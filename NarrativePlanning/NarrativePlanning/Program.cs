8d7140b02858b153db2fdfb6ea445c577901300f
elliotta23 <elliotta23@students.ecu.edu>
2025-11-14 01:03:20 -0500

    Add mp_stealth.json. Does not successfully generate plan from domain due to domain errors.

--- NarrativePlanning/NarrativePlanning/Program.cs
+++ NarrativePlanning/NarrativePlanning/Program.cs
@@ -46,7 +46,7 @@ namespace NarrativePlanning
 
 
             ////////////////////////////////////////////////////////
-            DomainBuilder.JSONDomainBuilder j = new NarrativePlanning.DomainBuilder.JSONDomainBuilder("../../JSON Files/mpk_eval_task.json", new Preferences());
+            DomainBuilder.JSONDomainBuilder j = new NarrativePlanning.DomainBuilder.JSONDomainBuilder("../../JSON Files/mp_stealth.json", new Preferences());
 
             // SMALL TEST
             /*WorldState knowledgeSet = new WorldState();
